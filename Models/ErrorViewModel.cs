bd6c1e05da841686e105e41c6822875e485cb2b5
Dylan <dylan.reeder5@gmail.com>
2022-10-14 10:06:17 -0400

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web452_assignment1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
